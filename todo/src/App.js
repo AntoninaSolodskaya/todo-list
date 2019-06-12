ed4b6b1cfbe89c51feb9cc53c9273e51163dca5a
Antonina Solodskaya <mailstonya@gmail.com>
2019-06-12 09:46:33 +0300

    create structure folder

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  state = {
+    list: [],
+    rootInput: ""
+  };
+  render () {
+
+    const { rootInput } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="main">
+            <form>
+              <input
+                value={rootInput}
+              />
+              <button className="add">+</button>
+            </form>
+            
+          </div>
+        </header>
+      </div>
+    );
+  }  
+};
 
 export default App;
